25d9de3f9dd7fe7d0542cb8913243911d4294884
HippolyteÃ <hippolyteaubin@gmail.com>
2023-09-22 11:39:05 +0200

    PLayerRestController done

--- src/main/java/com/example/spring_exo1/controllers/PlayerRestController.java
+++ src/main/java/com/example/spring_exo1/controllers/PlayerRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,8 +20,36 @@ import java.util.UUID;
 public class PlayerRestController {
     private final PlayerService playerService;
 
+    @GetMapping
+    public List<PlayerDTO> listPLayer(@RequestParam(value = "pseudo", defaultValue = "") String filterByPseudo){
+        List<PlayerDTO> players = playerService.getPlayers();
 
-    @DeleteMapping("/delete/{playerId}")
+        if (!filterByPseudo.isEmpty() && !filterByPseudo.isBlank()) {
+            players = players.stream().filter(p -> p.getPseudo().startsWith(filterByPseudo)).toList();
+        }
+        return players;
+    }
+
+    @GetMapping("/{playerId}")
+    public PlayerDTO getPlayerDetails(@PathVariable("playerId") UUID id){
+        Optional<PlayerDTO> foundPlayer = playerService.getPlayerById(id);
+
+        if (foundPlayer.isPresent()){
+            return foundPlayer.get();
+        }
+        throw new RessourceNotFoundException();
+    }
+
+    @PostMapping("/add")
+    public  ResponseEntity<String> addPlayerHandler(@RequestBody PlayerDTO newPlayer){
+        PlayerDTO dto = playerService.addPlayer(newPlayer);
+
+        return new ResponseEntity<String>("Player was added, new id: " + dto.getId(), HttpStatus.CREATED);
+    }
+
+
+
+    @DeleteMapping("/{playerId}")
     public ResponseEntity<String> deletePlayerById(@PathVariable("playerId") UUID id) {
         Optional<PlayerDTO> foundPlayer = playerService.getPlayerById(id);
 
@@ -31,6 +60,17 @@ public class PlayerRestController {
             return new ResponseEntity<String>("Error while trying to delete player", HttpStatus.NOT_MODIFIED);
         } else {
             throw new RessourceNotFoundException();
+        }
+    }
+
+    @PatchMapping("/{playerId")
+    public  PlayerDTO editPlayer(@PathVariable("playerId") UUID id, @RequestBody PlayerDTO playerData) {
+        Optional<PlayerDTO> foundPlayer = playerService.getPlayerById(id);
+
+        if (foundPlayer.isPresent()){
+            return playerService.changePlayerPseudo(id, playerData);
+        } else {
+            throw  new RessourceNotFoundException();
         }
     }
 }
